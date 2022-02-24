753ea3e59a6dbd43a6b8f5e38fd5d860f5a9ea2f
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-02-24 16:28:08 +0900

    learning data type

--- src/chap02/lecture/C08TypeString.java
+++ src/chap02/lecture/C08TypeString.java
@@ -2,6 +2,17 @@ package chap02.lecture;
 
 public class C08TypeString {
 	public static void main(String[] args) {
-		
+		// 문자열 string
+		String var1 = "";
+		String var2 = "A";
+		String var3 = "hello world!";
+		String var4 = "hello \tworld";
+		String var5 = "hello\nworld";
+		String var6 = "I am \"ironman\"";
+
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
 	}
 }
