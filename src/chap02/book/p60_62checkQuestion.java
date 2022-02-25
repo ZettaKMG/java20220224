08b3db0ebc6efe71128a6e76cf0d5cbfa3290139
ZettaKMG <fin081128@naver.com>
2022-02-25 11:13:11 +0900

    Solving Chapter2 Check Question

--- src/chap02/book/p60_62checkQuestion.java
+++ src/chap02/book/p60_62checkQuestion.java
@@ -1,13 +1,13 @@
 package chap02.book;
 
-public class p60_61checkQuestion {
+public class p60_62checkQuestion {
 	public static void main(String[] args) {
 		
 //		1. 자바에서 변수에 대한 설명 중 틀린 것은 무엇입니까? (x, 정답 4번)
 //			2) 변수는 선언 시에 사용한 타입의 값만 저장할 수 있다.
 //			정답 : 4) 변수는 초기값이 저장되지 않은 상태에서 읽을 수 있다.
 //
-//		2. 변수 이름으로 사용 가능한 것을 모두 선택하세요.
+//		2. 변수 이름으로 사용 가능한 것을 모두 선택하세요. (ㅇ)
 //			1) modelName, 4) $value, 5)_age
 //
 //		3. 다음 표의 빈칸에 자바의 기본타입(Primitive Type) 8개를 적어보세요.
