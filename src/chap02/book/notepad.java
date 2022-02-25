2dd9ef8b809b3d79e857e8ce7beb74be0ecc8bbd
ZettaKMG <fin081128@naver.com>
2022-02-25 11:09:44 +0900

    Solving chapter2 Check Question

---
+++ src/chap02/book/notepad.java
@@ -0,0 +1,29 @@
+package chap02.book;
+
+public class notepad {
+	public static void main(String[] args) {
+//		byte byteValue = 10;
+//		char charValue = 'A';
+		
+//		int intValue = byteValue;
+//		int intValue = charValue;
+//		short shortValue = charValue;
+//		double doubleValue = byteValue;
+		
+//		int intValue = 10;
+//		char charValue = 'A';
+//		double doubleValue = 5.7;
+//		String strValue = "A";
+		
+//		double var = (double) intValue;
+//		byte var = (byte) intValue;
+//		int var = (int) doubleValue;
+//		char var = (char) strValue;
+		
+//		int var1 = 10;
+//		long var2 = 10000000000L;
+//		char var3 = '';
+//		double var4 = 10;
+//		float var5 = 10;
+	}
+}
