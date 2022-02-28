e60f6abf5ad4e595829ebc51ff9398cc82ee0cb0
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-02-28 12:12:31 +0900

    learning if-else

---
+++ src/chap04/lecture/ifelse/C01if.java
@@ -0,0 +1,13 @@
+package chap04.lecture.ifelse;
+
+public class C01if {
+	public static void main(String[] args) {
+		System.out.println("statement1");
+		
+		if (false) {
+		System.out.println("statement2");
+		System.out.println("statement3");
+		}
+		System.out.println("statement4");
+	}
+}
---
+++ src/chap04/lecture/ifelse/C02else.java
@@ -0,0 +1,7 @@
+package chap04.lecture.ifelse;
+
+public class C02else {
+	public static void main(String[] args) {
+		
+	}
+}
