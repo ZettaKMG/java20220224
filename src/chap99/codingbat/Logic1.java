0834e42fc1c6df5f017e993c7a02a72b19f887ec
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-02-28 16:16:31 +0900

    practice return if-else switch

--- src/chap99/codingbat/Logic1.java
+++ src/chap99/codingbat/Logic1.java
@@ -59,4 +59,33 @@ public class Logic1 {
 		}
 	}
 
+	public String alarmClock(int day, boolean vacation) {
+		String early;
+		String late;
+
+		if (vacation) {
+			early = "10:00";
+			late = "off";
+		} else {
+			early = "7:00";
+			late = "10:00";
+		}
+
+		String result = "";
+		switch (day) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			result = early;
+			break;
+		case 0:
+		case 6:
+			result = late;
+		}
+
+		return result;
+	}
+
 }
