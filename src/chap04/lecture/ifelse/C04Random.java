685c7240a8a161270c84b6c5ce394c9826ead7ff
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-02-28 16:15:37 +0900

    learning random

--- src/chap04/lecture/ifelse/C04Random.java
+++ src/chap04/lecture/ifelse/C04Random.java
@@ -6,19 +6,23 @@ public class C04Random {
 		System.out.println("1~10 임의의 자연수");
 		double n1 = Math.random(); // 0.0 <= n < 1.0
 		System.out.println(n1);
-		
+
 		double n2 = n1 * 10;
 		System.out.println(n2);
-		
+
 		int n3 = (int) n2;
 		System.out.println(n3);
-		
+
 		int n4 = n3 + 1;
 		System.out.println(n4);
-		
+
 		// 주사위
 		System.out.println("주사위");
-		System.out.println(((int) (Math.random()*6)) + 1);
-	
+		System.out.println(((int) (Math.random() * 6)) + 1);
+
+		// 로또 (1~45)번호
+		System.out.println("로또");
+		System.out.println(((int) (Math.random() * 45) + 1));
+
 	}
 }
