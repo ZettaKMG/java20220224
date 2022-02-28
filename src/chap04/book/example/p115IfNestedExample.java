c33f02cce8f827a6cba7e69da4ece24f588046ae
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-02-28 16:51:29 +0900

    practice if-else

--- src/chap04/book/example/p115IfNestedExample.java
+++ src/chap04/book/example/p115IfNestedExample.java
@@ -2,14 +2,25 @@ package chap04.book.example;
 
 public class p115IfNestedExample {
 	public static void main(String[] args) {
-		int score = (int)(Math.random()*20)+81;
-		
-		System.out.println("점수: "+score);
-		
+		int score = (int) (Math.random() * 20) + 81;
+
+		System.out.println("점수: " + score);
+
 		String grade;
-		
-		if(score>=90) {
-			
+
+		if (score >= 90) {
+			if (score >= 95) {
+				grade = "A+";
+			} else {
+				grade = "A";
+			}
+		} else {
+			if (score >= 85) {
+				grade = "B+";
+			} else {
+				grade = "B";
+			}
 		}
+		System.out.println("학점: " + grade);
 	}
 }
