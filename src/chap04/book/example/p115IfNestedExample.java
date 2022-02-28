20d256b4bebc022739922efe1b2905f7744ebe01
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-02-28 16:16:59 +0900

    practice if-elseif

---
+++ src/chap04/book/example/p115IfNestedExample.java
@@ -0,0 +1,15 @@
+package chap04.book.example;
+
+public class p115IfNestedExample {
+	public static void main(String[] args) {
+		int score = (int)(Math.random()*20)+81;
+		
+		System.out.println("점수: "+score);
+		
+		String grade;
+		
+		if(score>=90) {
+			
+		}
+	}
+}
