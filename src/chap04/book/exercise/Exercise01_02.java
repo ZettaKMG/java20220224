88d71b602afac90353adaa1b87d3a108de88e41c
ZettaKMG <fin081128@naver.com>
2022-03-02 11:10:20 +0900

    Solving Chapter4 Exercise

---
+++ src/chap04/book/exercise/Exercise01_02.java
@@ -0,0 +1,12 @@
+package chap04.book.exercise;
+
+public class Exercise01_02 {
+	public static void main(String[] args) {
+//		1.
+//		조건문: if, switch
+//		반복문: for, while, do-while
+//			
+//		2. 조건문과 반복문을 설명한 것 중 틀린 것은 무엇입니까?
+//		  3) for문은 카운터 변수로 지정한 횟수만큼 반복시킬 때 사용할 수 있다.
+	}
+}
