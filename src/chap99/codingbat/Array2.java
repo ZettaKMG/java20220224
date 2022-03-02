77ed07e7484cdc9bfc8ad926dac7c0c3f87753de
ZettaKMG <fin081128@naver.com>
2022-03-02 16:30:15 +0900

    practicing array

--- src/chap99/codingbat/Array2.java
+++ src/chap99/codingbat/Array2.java
@@ -2,14 +2,89 @@ package chap99.codingbat;
 
 public class Array2 {
 	public int countEvens(int[] nums) {
-		  int cnt = 0;
+		int cnt = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if ((nums[i] % 2) == 0) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	public int bigDiff(int[] nums) {
+		int max = nums[0];
+		int min = nums[0];
+
+		for (int i = 0; i < nums.length; i++) {
+			if (max < nums[i]) {
+				max = nums[i];
+			}
+		}
+
+		for (int j = 0; j < nums.length; j++) {
+			if (min > nums[j]) {
+				min = nums[j];
+			}
+		}
+
+		return max - min;
+	}
+
+	public int centeredAverage(int[] nums) {
+		int sum = 0;
+		int max = nums[0];
+		int min = nums[0];
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (max < nums[i]) {
+				max = nums[i];
+			}
+
+			if (min > nums[i]) {
+				min = nums[i];
+			}
+
+			sum += nums[i];
+		}
+		return (sum - max - min) / (nums.length - 2);
+	}
+
+	public int centeredAverage1(int[] nums) {
+		int sum = 0;
+		int max = nums[0];
+		int min = nums[0];
+
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			max = (max < nums[i]) ? nums[i] : max;
+			min = (min > nums[i]) ? nums[i] : min;
+		}
+
+		sum -= max;
+		sum -= min;
+
+		return sum / (nums.length - 2);
+	}
+	
+	public int sum13(int[] nums) {
+		  int sum = 0;
+		  boolean is13 = false;
+		  boolean was13 = false;
 		  
 		  for (int i = 0; i < nums.length; i++){
-		    if ((nums[i] % 2) == 0){
-		      cnt++;
+		    is13 = nums[i] == 13;
+		    
+		    if (!was13 && !is13){
+		    sum += nums[i];
 		    }
+		    
+		    was13 = is13;
 		  }
-		  return cnt;
+		  
+		  return sum;
 		}
+
 
 }
