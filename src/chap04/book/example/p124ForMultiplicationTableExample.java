71b7ae17020be243c99fb95bc62e3d2f01ef586a
ZettaKMG <fin081128@naver.com>
2022-03-02 11:51:38 +0900

    practice for

--- src/chap04/book/example/p124ForMultiplicationTableExample.java
+++ src/chap04/book/example/p124ForMultiplicationTableExample.java
@@ -8,5 +8,22 @@ public class p124ForMultiplicationTableExample {
 				System.out.println(m + " x " + n + " = " + (m * n));
 			}
 		}
+
+		// 9단 ~ 2단
+		System.out.println("############# 9단 ~ 2단");
+		for (int i = 9; i >= 2; i--) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + " x " + j + " = " + (i * j));
+			}
+		}
+
+		// 9 X 9 = 81 ~~~~ 2 X 1 = 2
+		System.out.println("############# 9 X 9 = 81 ~ 2 X 1 = 2");
+		for (int k = 9; k >= 2; k--) {
+			for (int l = 9; l >= 1; l--) {
+				System.out.println(k + " x " + l + " = " + (k * l));
+			}
+		}
+
 	}
 }
