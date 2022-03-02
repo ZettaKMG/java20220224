a273e8cf388e40a8f9d97b0cc76c8ffd668aa0d7
ZettaKMG <fin081128@naver.com>
2022-03-02 12:40:43 +0900

    Solving Chapter4 Exercise

--- src/chap04/book/exercise/Exercise06.java
+++ src/chap04/book/exercise/Exercise06.java
@@ -14,8 +14,37 @@ public class Exercise06 {
 		
 		System.out.print("출력1 ");
 		System.out.print("출력2 ");
+		System.out.println(); // 그냥 다음 줄
 		System.out.print("출력3 ");
 		System.out.println("출력4");
 		System.out.println("출력5");
+		
+		System.out.print("*");
+		System.out.println();
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");		
+		System.out.println();
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < (i+1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
