c7b280b9a4f7c91a173c1c2bbcb0de5ee8a6351b
ZettaKMG <fin081128@naver.com>
2022-03-02 12:08:55 +0900

    Solving Chapter4 Exercise

--- src/chap04/book/exercise/Exercise06.java
+++ src/chap04/book/exercise/Exercise06.java
@@ -3,8 +3,19 @@ package chap04.book.exercise;
 public class Exercise06 {
 	public static void main(String[] args) {
 		// 작성 위치
-		for (int i = 1; i <= 5; i++) {
-			for 
+		String star = "*";
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(star);
+			star += "*";
 		}
+		
+		System.out.println("출력 (끝에 enter 없이)");
+		
+		System.out.print("출력1 ");
+		System.out.print("출력2 ");
+		System.out.print("출력3 ");
+		System.out.println("출력4");
+		System.out.println("출력5");
 	}
 }
