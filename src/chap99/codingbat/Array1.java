35c63279e9c47aa69fe93b1186145ab7440e2dba
ZettaKMG <fin081128@naver.com>
2022-03-02 14:40:33 +0900

    practice array

---
+++ src/chap99/codingbat/Array1.java
@@ -0,0 +1,21 @@
+package chap99.codingbat;
+
+public class Array1 {
+	public boolean firstLast6(int[] nums) {
+		  
+		  return ((nums[0] == 6) || (nums[nums.length-1]) == 6);
+		}
+	
+	public boolean sameFirstLast(int[] nums) {
+		  
+		  if (nums.length > 0) {
+		  return nums[0] == nums[nums.length-1];
+		  }
+		  
+		  return false;
+		}
+	
+	public boolean commonEnd(int[] a, int[] b) {
+		  return ((a[0] == b[0]) || (a[a.length-1] == b[b.length-1]));
+		}
+}
