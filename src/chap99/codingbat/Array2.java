8cbdec8c6ef26a7db6ee08f9e10916641818dd4e
ZettaKMG <fin081128@naver.com>
2022-03-04 12:31:42 +0900

    practice array

--- src/chap99/codingbat/Array2.java
+++ src/chap99/codingbat/Array2.java
@@ -177,4 +177,15 @@ public class Array2 {
 		return arr;
 	}
 
+	public int[] shiftLeft(int[] nums) {
+		int[] result = new int[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+
+			result[i] = nums[(i + 1) % nums.length];
+		}
+
+		return result;
+	}
+
 }
