0d908b67e547cdc3d1b9e7adb70ff127a0c41b7d
ZettaKMG <fin081128@naver.com>
2022-03-04 10:25:13 +0900

    learn reference type

---
+++ src/chap05/lecture/reftype/C01ReferenceType.java
@@ -0,0 +1,13 @@
+package chap05.lecture.reftype;
+
+public class C01ReferenceType {
+	public static void main(String[] args) {
+		int i1;
+		
+		String s1;
+		s1 = "hello";
+		
+		int i2 = 5;
+		String s2 = "java";
+	}
+}
---
+++ src/chap05/lecture/reftype/C05ReferenceType.java
@@ -0,0 +1,7 @@
+package chap05.lecture.reftype;
+
+public class C05ReferenceType {
+	public static void main(String[] args) {
+		
+	}
+}
