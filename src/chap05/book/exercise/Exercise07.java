bf023496264ba6bce628ed03b0175decddf6b459
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-07 09:49:08 +0900

    Solving Chapter5 Exercise

--- src/chap05/book/exercise/Exercise07.java
+++ src/chap05/book/exercise/Exercise07.java
@@ -2,8 +2,9 @@ package chap05.book.exercise;
 
 public class Exercise07 {
 	public static void main(String[] args) {
-		int max = 0;
-		int[] array = { 1, 5, 3, 8, 2 };
+		int[] array = { -1, -5, -3, -8, -2 };
+//		int max = array[0];
+		int max = Integer.MIN_VALUE;
 		
 		// 작성 위치
 		for(int i = 0; i < array.length; i++) {
