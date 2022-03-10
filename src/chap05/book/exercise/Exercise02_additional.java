d9d33af2272681d5b0c0789a5090b1dc3e366c44
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-10 09:53:40 +0900

    Solving Chapter5 Exercise

---
+++ src/chap05/book/exercise/Exercise02_additional.java
@@ -0,0 +1,10 @@
+package chap05.book.exercise;
+
+public class Exercise02_additional {
+	public static void main(String[] args) {
+		int[] arr1 = {3, 2, 1};
+		arr1 = new int[] {99, 88};
+		
+		// {3, 2, 1} 인스턴스는 언제가 Garbage Collector(gc)에 의해 메모리에서 삭제됨.
+	}
+}
--- src/chap05/book/exercise/Exercise07.java
+++ src/chap05/book/exercise/Exercise07.java
@@ -3,19 +3,23 @@ package chap05.book.exercise;
 public class Exercise07 {
 	public static void main(String[] args) {
 		int[] array = { -1, -5, -3, -8, -2 };
-//		int max = array[0];
-		int max = Integer.MIN_VALUE;
-		
+		int max = array[0];
+//		int max = Integer.MIN_VALUE;
+
 		// 작성 위치
-		for(int i = 0; i < array.length; i++) {
-			if ( max < array[i] ) {
-				max = array[i];
-			} else {
-				max = max;
+		/*
+		 * for(int i = 0; i < array.length; i++) { if ( max < array[i] ) { max =
+		 * array[i]; } else { max = max; } }
+		 */
+		
+		// 향상된 for문 사용
+		for (int item : array) {
+			if (item > max) {
+				max = item;
 			}
 		}
-		
+
 		System.out.println("max: " + max);
-		
+
 	}
 }
