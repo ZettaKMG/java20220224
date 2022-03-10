ba02ccd4570bb73886ee8e4a846683702b2b5d5b
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-10 17:53:34 +0900

    practice array

---
+++ src/chap99/leetcode/Solution1480.java
@@ -0,0 +1,19 @@
+package chap99.leetcode;
+
+public class Solution1480 {
+	class Solution {
+		public int[] runningSum(int[] nums) {
+			int[] answer = new int[nums.length];
+
+			for (int i = 0; i < nums.length; i++) {
+				if (i == 0) {
+					answer[i] = nums[i];
+				} else {
+					answer[i] = answer[i - 1] + nums[i];
+				}
+			}
+
+			return answer;
+		}
+	}
+}
