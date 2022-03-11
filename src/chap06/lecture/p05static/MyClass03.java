6ab85d06ce60a7df3298b00a87d8a7d51f9a9818
ZettaKMG <fin081128@naver.com>
2022-03-11 15:34:47 +0900

    learn static

---
+++ src/chap06/lecture/p05static/MyClass03.java
@@ -0,0 +1,19 @@
+package chap06.lecture.p05static;
+
+public class MyClass03 {
+	static {
+		// static block
+		// 정적 초기화 블럭
+		for (int i = 0; i < 10; i++) {
+			sum += i;
+		}
+		field2 = sum;
+	}
+	
+	static int field2;
+	int field1;
+	
+	MyClass03(){
+		field1 = 10;
+	}
+}
