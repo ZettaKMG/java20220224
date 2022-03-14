b90e3a010477b3dd90371725439d897ef4fe2e70
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-14 16:33:47 +0900

    practice access modifier

--- src/chap06/book/example/s061302/package2/C.java
+++ src/chap06/book/example/s061302/package2/C.java
@@ -4,8 +4,8 @@ import chap06.book.example.s061302.package1.A;
 
 public class C {
 	// 필드
-	A a1 = new A(true);     // (o)
-	A a2 = new A(1);        // (x)
-	A a3 = new A("문자열");   // (x)
+	A a1 = new A(true);		// (o)
+	A a2 = new A(1);		// (x)
+	A a3 = new A("문자열");	// (x)
 
 }
--- src/chap06/book/example/s061303/package2/C.java
+++ src/chap06/book/example/s061303/package2/C.java
@@ -5,12 +5,12 @@ import chap06.book.example.s061303.package1.A;
 public class C {
 	public C() {
 		A a = new A();
-		a.field1 = 1;
-		a.field2 = 1;
-		a.field3 = 1;
+		a.field1 = 1;	// (o)
+		a.field2 = 1;	// (x)
+		a.field3 = 1;	// (x)
 		
-		a.method1();
-		a.method2();
-		a.method3();		
+		a.method1();	// (o)
+		a.method2();	// (x)
+		a.method3();	// (x)		
 	}
 }
