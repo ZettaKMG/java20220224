8d89b5be044b6f7ecb51bd1584f2083ef195ea97
ZettaKMG <fin081128@naver.com>
2022-03-15 12:20:48 +0900

    solve ch7 exercise

---
+++ src/chap07/book/exercise/p05/Child.java
@@ -0,0 +1,11 @@
+package chap07.book.exercise.p05;
+
+public class Child extends Parent {
+	private int studentNo;
+	
+	public Child(String name, int studentNo) {
+//		super("param");
+		this.name = name;
+		this.studentNo = studentNo;
+	}
+}
