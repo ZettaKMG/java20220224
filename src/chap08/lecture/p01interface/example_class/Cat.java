78234ab24e9ed5d8f10caceb59f7c3a3a37bee4d
ZettaKMG <fin081128@naver.com>
2022-03-17 11:35:59 +0900

    learn interface

---
+++ src/chap08/lecture/p01interface/example_class/Cat.java
@@ -0,0 +1,14 @@
+package chap08.lecture.p01interface.example_class;
+
+public class Cat extends Feline implements Pet {
+
+	@Override
+	public void cry() {
+		System.out.println("야옹~");
+	}
+	
+	@Override
+	public void roll() {
+		System.out.println("고양이가 무시합니다.");		
+	}
+}
---
+++ src/chap08/lecture/p01interface/example_class/Pet.java
@@ -0,0 +1,9 @@
+package chap08.lecture.p01interface.example_class;
+
+public interface Pet {
+	// 인터페이스명 : UpperCamelCase
+	// interface 키워드 사용
+	
+	void roll(); // public abstract 생략되어 있음		
+}
+
