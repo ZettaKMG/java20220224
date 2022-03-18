f1c4abcedfc8aee8ec8a4b471e947f9883f691a4
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-18 12:09:52 +0900

    solve ch8 exercise

--- src/chap09/book/exercise/p06/Chatting.java
+++ src/chap09/book/exercise/p06/Chatting.java
@@ -2,6 +2,17 @@ package chap09.book.exercise.p06;
 
 public class Chatting {
 	void startChat(String chatId) {
+//		class MyChat extends Chat{
+//			@Override
+//			void start() {
+//				System.out.println("스타트메소드 재정의");
+//			}
+//		}
+//		
+//		Chat chat = new MyChat();
+//		chat.start();
+		// ----- 상속을 통한 코드 작성시 -----
+		
 		String nickName = null;
 		nickName = chatId;
 		Chat chat = new Chat() {
