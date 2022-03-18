c7654d8439bfd839c0e66c4ccf18c0ed680e9324
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-18 10:06:36 +0900

    learn nested class

---
+++ src/chap09/lecture/p01nested/MyClass5.java
@@ -0,0 +1,17 @@
+package chap09.lecture.p01nested;
+
+public class MyClass5 {
+	void method1() {
+		// local class
+		class LocalClass1 {
+			int field1;
+			static int field2; // (x)
+			
+			void method1() {
+				
+			}
+			
+			static void method2() { // (x)
+		}
+	}
+}
---
+++ src/chap09/lecture/p01nested/MyClass6.java
@@ -0,0 +1,11 @@
+package chap09.lecture.p01nested;
+
+public class MyClass6 {
+	void method1() {
+		class LocalClass{
+			void method1() {
+				
+			}
+		}
+	}
+}
