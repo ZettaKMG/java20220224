b4db931d155ed15bced424e8e925472561ae05a1
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-18 12:23:15 +0900

    solve ch9 exercise

---
+++ src/chap09/book/exercise/p05/Anonymous.java
@@ -0,0 +1,15 @@
+package chap09.book.exercise.p05;
+
+public class Anonymous {
+	Vehicle field = 
+			
+	void method1() {
+		Vehicle localVar =
+				
+		localVar.run();
+	}
+	
+	void method2(Vehicle v) {
+		v.run();
+	}
+}
