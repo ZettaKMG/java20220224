a23cdee23f106f04be5310151441cae514a1a186
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-18 16:27:19 +0900

    practice nested class

--- src/chap09/book/example/s090304/Outter.java
+++ src/chap09/book/example/s090304/Outter.java
@@ -7,6 +7,17 @@ public class Outter {
 	}
 	
 	class Nested {
-		
+		String field = "Nested-field";
+		void method() {
+			System.out.println("Nested-method");
+		}
+		void print() {
+			System.out.println(this.field);
+			this.method();
+			// ----- 중첩 객체 참조 -----
+			System.out.println(Outter.this.field);
+			Outter.this.method();
+			// ----- 바깥 객체 참조 -----
+		}
 	}
 }
