03f846a97ed79b25a1b34025a8a0670d9942ac9c
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-18 16:00:30 +0900

    practice nested class

--- src/chap09/book/example/s090303/Outter.java
+++ src/chap09/book/example/s090303/Outter.java
@@ -1,5 +1,27 @@
 package chap09.book.example.s090303;
 
 public class Outter {
+	// 자바 7 이전
+	public void method1(final int arg) {
+		final int localVariable = 1;
+		// arg = 100; (x)
+		// localVariable = 100; (x)
+		class Inner {
+			public void method() {
+				int result = arg + localVariable;
+			}
+		}
+	}
 	
+	// 자바 8 이후
+	public void method2(int arg) {
+		int localVariable = 1;
+		// arg = 100; (x)
+		// localVariable = 100; (x)
+		class Inner {
+			public void method() {
+				int result = arg + localVariable;
+			}
+		}
+	}
 }
