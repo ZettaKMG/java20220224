b93358a6f68fd6e7890b5a6ea9d4d407592b9a5f
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-22 11:22:06 +0900

    solve ch11 exercise

--- src/chap11/book/exercise/p01_02.java
+++ src/chap11/book/exercise/p01_02.java
@@ -1,4 +1,4 @@
-package chap11.book.exercise.p04;
+package chap11.book.exercise;
 
 public class p01_02 {
 //	1. Object 클래스에 대한 설명 중 틀린 것은 무엇입니까?
@@ -8,5 +8,5 @@ public class p01_02 {
 //	  Hashtable을 사용하려고 합니다. Object의 equals()와 hashCode() 
 //	  메소드를 오버라이딩했다고 가정할 경우, 메소드 호출 순서를 생각하고 다음 괄호 ( )
 //	  안을 채워보세요.
-//	  (), ()
+//	  ( hashCode ), ( equals )
 }
