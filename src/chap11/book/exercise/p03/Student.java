359b218114237dfb915ba99d9a2e5f48d281eec6
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-03-22 11:15:15 +0900

    solve ch11 exercise

---
+++ src/chap11/book/exercise/p03/Student.java
@@ -0,0 +1,5 @@
+package chap11.book.exercise.p03;
+
+public class Student {
+
+}
---
+++ src/chap11/book/exercise/p04/p01_02.java
@@ -0,0 +1,12 @@
+package chap11.book.exercise.p04;
+
+public class p01_02 {
+//	1. Object 클래스에 대한 설명 중 틀린 것은 무엇입니까?
+//		
+//			
+//	2. 여러분이 작성하는 클래스를 동등 비교하는 컬렉션 객체인 HashSet, HashMap, 
+//	  Hashtable을 사용하려고 합니다. Object의 equals()와 hashCode() 
+//	  메소드를 오버라이딩했다고 가정할 경우, 메소드 호출 순서를 생각하고 다음 괄호 ( )
+//	  안을 채워보세요.
+//	  (), ()
+}
