3413cda37f5867c178676f5eb76863381493d89f
ZettaKMG <fin081128@naver.com>
2022-03-25 12:21:36 +0900

    learn arraylist foreach

--- src/chap14/lecture/p01lambda/App08.java
+++ src/chap14/lecture/p01lambda/App08.java
@@ -13,5 +13,14 @@ public class App08 {
 		list1.add("friday");
 		
 		list1.forEach(e -> System.out.println(e));
+		
+		// 절반의 문자열 출력
+		// 향상된 for문 사용
+//		for (String s : list1) {
+//			System.out.println(s.substring(0, s.length() / 2));
+//		}
+		
+		// forEach 사용
+		list1.forEach(s -> System.out.println(s.substring(0, s.length()/2)));
 	}
 }
