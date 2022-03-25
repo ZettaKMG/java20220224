2904050751c7e45a3fb87598fbf84f2376d5f674
ZettaKMG <fin081128@naver.com>
2022-03-25 16:34:33 +0900

    solve codingbat

--- src/chap99/codingbat/Functional2.java
+++ src/chap99/codingbat/Functional2.java
@@ -24,5 +24,33 @@ public class Functional2 {
 		  
 		  return nums;
 		}
+	
+	public List<String> noZ(List<String> strings) {
+		strings.removeIf(e -> e.contains("z"));
+
+		return strings;
+	}
+	
+	public List<String> noLong(List<String> strings) {
+		// List<String> result = new ArrayList<>();
+
+		// for (String str : strings) {
+		//   if (str.length() < 4) {
+		//     result.add(str);
+		//   }
+		// }
+
+		// return result;
+
+		strings.removeIf(s -> s.length() >= 4);
+		return strings;
+	}
+	
+	public List<String> no34(List<String> strings) {
+		  strings.removeIf(s -> s.length() == 3 || s.length() == 4 );
+		  
+		  return strings;
+		}
+
 
 }
