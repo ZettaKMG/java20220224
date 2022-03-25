f044be352d1adbc08494a41302a532f4e8f19f4d
ZettaKMG <fin081128@naver.com>
2022-03-25 12:33:55 +0900

    learn arraylist replaceall

--- src/chap14/lecture/p01lambda/App09.java
+++ src/chap14/lecture/p01lambda/App09.java
@@ -1,7 +1,7 @@
 package chap14.lecture.p01lambda;
 
-import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public class App09 {
 	public static void main(String[] args) {
@@ -18,6 +18,12 @@ class MyArrayList {
 	public void forEach(Consumer<String> action) {
 		for (String v : values) {
 			action.accept(v);
+		}
+	}
+	
+	public void replaceAll(UnaryOperator<String> op) {
+		for (int i = 0; i < values.length; i++) {
+			values[i] = op.apply(values[i]);
 		}
 	}
 }
