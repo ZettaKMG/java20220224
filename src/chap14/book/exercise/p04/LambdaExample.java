35890ae474742b584aac4117ac684bc78ab069d3
ZettaKMG <fin081128@naver.com>
2022-03-25 11:34:10 +0900

    solve ch14 exercise

--- src/chap14/book/exercise/p04/LambdaExample.java
+++ src/chap14/book/exercise/p04/LambdaExample.java
@@ -4,11 +4,9 @@ import java.util.function.IntSupplier;
 
 public class LambdaExample {
 	public static int method(int x, int y) {
-		IntSupplier supplier = () -> {
-			x *= 10;
-			int result = x + y;
-			return result;
-		};
+		IntSupplier supplier = () -> (x * 10) + y;
+//			x *= 10; // final or effectively final 이어야 함
+			
 		int result = supplier.getAsInt();
 		return result;
 	}
