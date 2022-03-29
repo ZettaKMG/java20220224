60ac26af08db9b22e1eed69d393542a2638e283d
ZettaKMG <fin081128@naver.com>
2022-03-29 12:17:28 +0900

    practice hashset

---
+++ src/chap15/book/example/s150301/HashSetExample2.java
@@ -0,0 +1,16 @@
+package chap15.book.example.s150301;
+
+import java.util.Set;
+
+import chap11.book.example.s110301.Member;
+
+public class HashSetExample2 {
+	public static void mainMemberg[] args) {
+		Set<Member> set = new HashSet<Member>();
+		
+		set.add(new Member("홍길동", 30)); // 인스턴스는 다르지만 내부 데이터가
+		set.add(new Member("홍길동", 30)); // 동일하므로 객체 1개만 저장
+		
+		System.out.println("총 객체수 : " + set.size()); // 저장된 객체 수 얻기
+	}
+}
