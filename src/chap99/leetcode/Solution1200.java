790b5557f564447f0c075bae9e297dd669ec29d7
ZettaKMG <fin081128@naver.com>
2022-03-29 16:37:20 +0900

    solve leetcode

--- src/chap99/leetcode/Solution1200.java
+++ src/chap99/leetcode/Solution1200.java
@@ -6,23 +6,23 @@ import java.util.List;
 
 public class Solution1200 {
 	public List<List<Integer>> minimumAbsDifference(int[] arr) {
-	       Arrays.sort(arr);
-	        
-	        int minDiff = Integer.MAX_VALUE;
-	        for( int i = 0; i < arr.length - 1; i++) {
-	            int diff = arr[i+1] - arr[i];
-	            minDiff = Math.min(minDiff, diff);
-	        }
-	        
-	        List<List<Integer>> res = new ArrayList<>();
-	        
-	        for ( int i = 0; i < arr.length - 1; i++) {
-	            int diff = arr[i+1] - arr[i];
-	            if (minDiff == diff) {
-	                res.add(Arrays.asList(arr[i], arr[i+1]));
-	            }
-	        }
-	        
-	        return res;
-	    }  
+		Arrays.sort(arr);
+
+		int minDiff = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length - 1; i++) {
+			int diff = arr[i + 1] - arr[i];
+			minDiff = Math.min(minDiff, diff);
+		}
+
+		List<List<Integer>> res = new ArrayList<>();
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			int diff = arr[i + 1] - arr[i];
+			if (minDiff == diff) {
+				res.add(Arrays.asList(arr[i], arr[i + 1]));
+			}
+		}
+
+		return res;
+	}
 }
