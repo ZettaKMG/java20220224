fec8281bad415e2ba1ba408b63a5790b3c9e9d08
ZettaKMG <fin081128@naver.com>
2022-03-31 15:32:53 +0900

    solve leetcode

--- src/chap99/leetcode/Solution1748.java
+++ src/chap99/leetcode/Solution1748.java
@@ -4,21 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution1748 {
+//	public int sumOfUnique(int[] nums) {
+//		Map<Integer, Integer> map = new HashMap<>();
+//		for (int n : nums) {
+//			map.put(n, map.getOrDefault(n, 0) + 1);
+//		}
+//
+//		int sum = 0;
+//		for (int n : map.keySet()) {
+//			if (map.get(n) == 1) {
+//				sum += n;
+//			}
+//		}
+//		return sum;
+//	}
+
 	public int sumOfUnique(int[] nums) {
 		Map<Integer, Integer> map = new HashMap<>();
-		for (int n : nums) {
-			map.put(n, map.getOrDefault(n, 0) + 1);
+		for (int num : nums) {
+			map.compute(num, (k, v) -> v == null ? 1 : v + 1);
 		}
 
-		int sum = 0;
-		for (int n : map.keySet()) {
-			if (map.get(n) == 1) {
-				sum += n;
+		int result = 0;
+
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if (entry.getValue() == 1) {
+				result += entry.getKey();
 			}
 		}
-		return sum;
+		return result;
 	}
-	
-	
 
 }
