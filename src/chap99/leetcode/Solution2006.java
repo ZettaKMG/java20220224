72f1172858aef1798e9d18e7aa43f78f9ca21dbd
ZettaKMG <fin081128@naver.com>
2022-03-31 16:39:24 +0900

    solve leetcode

--- src/chap99/leetcode/Solution2006.java
+++ src/chap99/leetcode/Solution2006.java
@@ -1,5 +1,37 @@
 package chap99.leetcode;
 
-public class Solution2006 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Solution2006 {
+//	public int countKDifference(int[] nums, int k) {
+//		Map<Integer, Integer> map = new HashMap<>();
+//
+//		int result = 0;
+//		for (int n : nums) {
+//			result += map.getOrDefault(n - k, 0) + map.getOrDefault(n + k, 0);
+//			map.merge(n, 1, Integer::sum);
+//		}
+//		return result;
+//	}
+
+	public int countKDifference(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int num : nums) {
+			map.compute(num, (key, v) -> v == null ? 1 : v + 1);
+		}
+
+		int result = 0;
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			Integer target = entry.getKey() + k;
+
+			if (map.containsKey(target)) {
+				result += entry.getValue() * map.get(target);
+			}
+		}
+
+		return result;
+
+	}
 }
--- src/chap99/leetcode/Solution2206.java
+++ src/chap99/leetcode/Solution2206.java
@@ -1,5 +1,38 @@
 package chap99.leetcode;
 
-public class Solution2206 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Solution2206 {
+//	public boolean divideArray(int[] nums) {
+//		Map<Integer, Integer> map = new HashMap<>();
+//		for (int num : nums) {
+//			map.put(num, map.getOrDefault(num, 0) + 1);
+//		}
+//
+//		for (Integer key : map.keySet()) {
+//			if (map.get(key) % 2 != 0) {
+//				return false;
+//			}
+//		}
+//
+//		return true;
+//	}
+
+	public boolean divideArray(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int num : nums) {
+			map.compute(num, (k, v) -> v == null ? 1 : v + 1);
+		}
+
+		java.util.Collection<Integer> values = map.values();
+		for (Integer value : values) {
+			if (value % 2 == 1) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
