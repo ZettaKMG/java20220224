0772a8423913e1e3c88b5bb132423ac3981e8c87
ZettaKMG <fin081128@naver.com>
2022-03-31 15:19:43 +0900

    solve leetcode

--- src/chap99/leetcode/Solution1748.java
+++ src/chap99/leetcode/Solution1748.java
@@ -1,5 +1,24 @@
 package chap99.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution1748 {
+	public int sumOfUnique(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int n : nums) {
+			map.put(n, map.getOrDefault(n, 0) + 1);
+		}
+
+		int sum = 0;
+		for (int n : map.keySet()) {
+			if (map.get(n) == 1) {
+				sum += n;
+			}
+		}
+		return sum;
+	}
+	
+	
 
 }
