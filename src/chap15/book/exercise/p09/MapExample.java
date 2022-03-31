72be7dea3011691f96242d450a999611cef87475
ZettaKMG <fin081128@naver.com>
2022-03-31 09:34:27 +0900

    solve ch15 exercise

--- src/chap15/book/exercise/p09/MapExample.java
+++ src/chap15/book/exercise/p09/MapExample.java
@@ -16,13 +16,34 @@ public class MapExample {
 		int totalScore = 0;	// 점수 합계 저장
 		
 		// 작성 위치
-		Set
+		// 1. entrySet
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry);
+		}
+		
+		// 2. keySet
+		Set<String> keySet = map.keySet();
+		for (String key : keySet) {
+			System.out.println(key + "=" + map.get(key));
+		}
+		
+		// 3. forEach
+		map.forEach((k, v) -> System.out.println(k + "=" + v));
+		
+		// keySet 방법 사용
+		keySet = map.keySet();
+		for (String key : keySet) {
+			Integer value = map.get(key);
+
+			if (value > maxScore) {
+				name = key;
+				maxScore = value;
+			} 
+			totalScore += value;			
+		}
+		
 		System.out.println("평균점수: " + totalScore / map.size());
-		
-		
 		System.out.println("최고점수: " + maxScore);
-		
-		
 		System.out.println("최고점수를 받은 아이디: " + name);
 	}
 }
