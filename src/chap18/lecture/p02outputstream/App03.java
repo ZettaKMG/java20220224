c04c91b2947710c85a96c7e25dad32292b2eddf4
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-04-01 11:30:14 +0900

    learn outputstream

--- src/chap18/lecture/p02outputstream/App03.java
+++ src/chap18/lecture/p02outputstream/App03.java
@@ -6,28 +6,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class App02 {
+public class App03 {
 	public static void main(String[] args) {
-		String inputFile = "src/chap18/lecture/p02outputstream/App02.java";
-		String outputFile = "output/OutputStreamEx02.txt";
-
+		
+		String inputFile = "src/chap18/lecture/p02outputstream/App03.java";
+		String outputFile = "output/OutputStreamEx03.txt";
+		
 		try (
 				InputStream is = new FileInputStream(inputFile);
 				OutputStream os = new FileOutputStream(outputFile);) {
-
-			int data1 = is.read();
-			os.write(data1);
-
-			int data2 = is.read();
-			os.write(data2);
-
-			int data3 = is.read();
-			os.write(data3);
-
+			
+			int data = 0;
+			
+			while ((data = is.read()) != -1) {
+				os.write(data);
+			}
+			
 		} catch (IOException e) {
-			e.printStackTrace();
+			e. printStackTrace();
 		}
 		
-		System.out.println("프로그램 종료!");
+		System.out.println("프로그램 종료!!!");
 	}
 }
