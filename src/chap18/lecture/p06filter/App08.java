7172b9db6a2503ceae63fafe546448f020e84f69
ZettaKMG <fin081128@naver.com>
2022-04-04 11:22:14 +0900

    learn filter

--- src/chap18/lecture/p06filter/App08.java
+++ src/chap18/lecture/p06filter/App08.java
@@ -1,5 +1,32 @@
 package chap18.lecture.p06filter;
 
-public class App08 {
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
+public class App08 {
+	public static void main(String[] args) throws Exception {
+		String out = "output/FilterExample8.txt";
+		OutputStream os = new FileOutputStream(out);
+		// OutputStreamWriter로 변환
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		// BufferedWriter로 변환
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		// 천만개의 문자 파일에 쓰는 코드 작성
+		int data = 0;
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000000; i++) {
+			bw.write('가');
+		}		
+		long end = System.currentTimeMillis();
+		
+		System.out.println(end - start);
+		
+		// 스트림 닫기
+		bw.close();
+		osw.close();
+		os.close();
+	}
 }
