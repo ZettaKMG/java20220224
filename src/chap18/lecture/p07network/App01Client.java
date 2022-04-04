8443a45114292669c3e521e0eb1e876a56be05b1
ZettaKMG <fin081128@naver.com>
2022-04-04 14:49:00 +0900

    learn network

--- src/chap18/lecture/p07network/App01Client.java
+++ src/chap18/lecture/p07network/App01Client.java
@@ -1,5 +1,9 @@
 package chap18.lecture.p07network;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -10,6 +14,18 @@ public class App01Client {
 		socket.connect(new InetSocketAddress("172.30.1.9", 38080), 5000);
 		System.out.println("[클라이언트쪽 소켓 생성됨]");
 		
+		InputStream is = socket.getInputStream();
+		Reader rd = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(rd);
+		
+		char[] datas = new char[1024];
+		int len = br.read(datas);
+		
+		System.out.println(new String(datas, 0 ,len));
+		
+		br.close();
+		rd.close();
+		is.close();
 		if (socket != null) {
 			socket.close();
 		}
--- src/chap18/lecture/p07network/App01Server.java
+++ src/chap18/lecture/p07network/App01Server.java
@@ -1,5 +1,9 @@
 package chap18.lecture.p07network;
 
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,6 +17,18 @@ public class App01Server {
 		Socket socket = serverSocket.accept();
 		
 		System.out.println("[서버쪽 소켓 생성됨]");
+		
+		OutputStream os = socket.getOutputStream();
+		Writer wr = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(wr);
+		
+		bw.write("클라이언트님 반갑습니다.");
+		
+		bw.close();
+		wr.close();
+		os.close();
+		socket.close();
+		serverSocket.close();
 		
 		System.out.println("프로그램 실행 종료...");
 	}
