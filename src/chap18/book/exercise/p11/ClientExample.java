af4e5d815452ee20a93eee6d0dec548208c8b504
ZettaKMG <fin081128@naver.com>
2022-04-04 17:47:32 +0900

    solve ch18 exercise

--- src/chap18/book/exercise/p11/ClientExample.java
+++ src/chap18/book/exercise/p11/ClientExample.java
@@ -1,12 +1,13 @@
 package chap18.book.exercise.p11;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class ClientExample {
 	public static void main(String[] args) throws Exception {
-		Socket socket = new Socket("localhost", 5001);
+		Socket socket = new Socket("172.30.1.9", 5001);
 		OutputStream os = socket.getOutputStream();
 		
 		String filePath = "C:\\Users\\user\\Desktop\\work\\java\\workspace\\java20220224\\java20220224\\output\\다형성01.png";
@@ -14,18 +15,15 @@ public class ClientExample {
 		
 		String fileName = file.getName();
 		// 작성 위치1
-		
-		
-		
-		
+		FileInputStream fis = new FileInputStream(fileName);
+		byte[] datas = new byte[1000];
 		
 		System.out.println("[파일 보내기 시작] " + fileName);
 		// 작성 위치2
-		
-		
-		
-		
-		
+		int len = 0;
+		while ((len = fis.read(datas)) != -1) {
+			os.write(datas, 0, len);
+		}	
 		
 		os.flush();
 		System.out.println("[파일 보내기 완료]");
--- src/chap18/book/exercise/p11/ServerExample.java
+++ src/chap18/book/exercise/p11/ServerExample.java
@@ -1,5 +1,6 @@
 package chap18.book.exercise.p11;
 
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -8,7 +9,7 @@ import java.net.Socket;
 public class ServerExample {
 	public static void main(String[] args) throws Exception {
 		ServerSocket serverSocket = new ServerSocket();
-		serverSocket.bind(new InetSocketAddress("localhost", 5001));
+		serverSocket.bind(new InetSocketAddress("172.30.1.9", 5001));
 		
 		System.out.println("[서버 시작]");
 		
@@ -22,17 +23,16 @@ public class ServerExample {
 				int readByteCount = -1;
 				
 				// 작성 위치3
-				
+				String fileName = "C:/Temp/다형성01.png";
 				
 				fileName = fileName.trim();
 				
 				System.out.println("[파일 받기 시작] " + fileName);
 				// 작성 위치4
-				
-				
-				
-				
-				
+				FileOutputStream fos = new FileOutputStream(fileName);
+				while ((readByteCount = is.read(bytes)) != -1) {
+					fos.write(bytes, 0, readByteCount);
+				}				
 				
 				System.out.println("[파일 받기 완료]");
 				
